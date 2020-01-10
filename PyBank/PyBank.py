e08ce5715e53c5c7a41fe623e0e4a1fe928d7840
davidtaboh <davidtaboh@yahoo.com>
2020-01-10 18:04:31 -0500

    adding HW3

--- PyBank/PyBank.py
+++ PyBank/PyBank.py
@@ -5,22 +5,22 @@ import os
 
 #read file in dictionary format
 
-#variables
+file_to_read = os.path.join("budget_file.csv")
+file_to_write = os.path.join("budget_data.txt")
 
+with open(file_to_read) as budget_file:
+    budget_file = csv.reader(budget_file)
 
-
-  
-    
-
-with open(r"C:\Users\david\Desktop\budget_file.csv", 'r') as budget_file:
-    budget_file = csv.DictReader(budget_file)
+    header = next(budget_file)
 
 #variables
-    
+    first_row = next(budget_file)
     total_change_profits = 0
     greatest_increase = 0
     initial_profit = 0
     monthly_changes = []
+    prev_net = int(first_row[1])
+   
 
 #Total Month
 
@@ -28,44 +28,19 @@ with open(r"C:\Users\david\Desktop\budget_file.csv", 'r') as budget_file:
     date_list = [] 
 
     for line in budget_file:
-        date_list.append(line["Date"]) 
-
-    
-#Total Revenue
-
-        totalrevenue += int(line["Profit/Losses"])
-
+        date_list.append(line[0])
+        net_change =  int(line[1]) - prev_net 
+        prev_net = int(line[1])
+        monthly_changes = monthly_changes + [net_change]
+        totalrevenue =  totalrevenue + int(line[1])
         
-#Profit/Losses Average Change
-#average_revenue = totalrevenue/len(date_list)   
-         
-monthly_change_profits = totalrevenue - initial_profit
-# N/A = 38 million - 0
+print(len(date_list)+1)
+print(totalrevenue + prev_net)
+print(sum(monthly_changes) / len(monthly_changes))
+print(max(monthly_changes))   
+print(min(monthly_changes)) 
+   
 
-monthly_changes.append(monthly_change_profits)
-#list
+with open(file_to_write, "w") as txt_file:
+    txt_file.write("hello")
 
-total_change_profits += monthly_change_profits
-# 38 million
-
-initial_profit = totalrevenue
-# 38 million
-
-average_change_profits = (total_change_profits/86)
-
-
-#net_revenue = totalrevenue - sum(int("Profit/Losses" < 0))
-
-#Greatest Profit Increase
-
-#greatest_increase = max((line["Profit/Losses"])
-
-#Greatest Profit Decrease
-
-#greatest_decrease = min("Profit/Losses")
-
-
-print(len(date_list))
-print(totalrevenue)
-#print(average_revenue)
-print(average_change_profits)
